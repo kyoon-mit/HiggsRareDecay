ae76f9fadfb2ec29f51ca26ea9031b4188a64b5f
Kyungseop Yoon <kyoon@mit.edu>
2021-11-03 02:41:12 -0400

    updated example

--- fitting/simple_fitting.py
+++ fitting/simple_fitting.py
@@ -53,6 +53,10 @@ def fit (histogram, process_name, outfile_name):
     Returns:
         (None)
     """
+    # Create outfile
+    outfile = TFile(outfile_name, 'RECREATE')
+
+    # Create workspace
     w = RooWorkspace('workspace_' + process_name)
 
     # Create signal models
@@ -75,45 +79,119 @@ def fit (histogram, process_name, outfile_name):
 
     # Create data histogram
     data_hist = RooDataHist('data_' + process_name, process_name,
-                            RooArgList(w.var('mass')), histogram, 1.0)
+                            RooArgList(w.var('mass')), histogram)
+    w.Import(data_hist)
     
-    # Fit signal models
-    res_gauss_chi2       = w.pdf('gauss_chi2').chi2FitTo(data_hist, RooFit.Save())
-    res_breitwigner_chi2 = w.pdf('breitwigner_chi2').chi2FitTo(data_hist, RooFit.Save())
-    res_voigtian_chi2    = w.pdf('voigtian_chi2').chi2FitTo(data_hist, RooFit.Save())
-    res_crystalball_chi2 = w.pdf('crystalball_chi2').chi2FitTo(data_hist, RooFit.Save())
-    res_gauss_mle        = w.pdf('gauss_mle').fitTo(data_hist, RooFit.Save())
-    res_breitwigner_mle  = w.pdf('breitwigner_mle').fitTo(data_hist, RooFit.Save())
-    res_voigtian_mle     = w.pdf('voigtian_mle').fitTo(data_hist, RooFit.Save())
-    res_crystalball_mle  = w.pdf('crystalball_mle').fitTo(data_hist, RooFit.Save())
+    # Fit signal models & save snapshots
+    list_of_models_chi2 = ['gauss_chi2',
+                           'breitwigner_chi2',
+                           'voigtian_chi2',
+                           'crystalball_chi2']
+    list_of_models_mle  = ['gauss_mle',
+                           'breitwigner_mle',
+                           'voigtian_mle',
+                           'crystalball_mle']
+    
+    for model in list_of_models_chi2:
+        res = w.pdf(model).chi2FitTo(data_hist, RooFit.Save())
+        params = w.pdf(model).getParameters(RooArgSet(w.var('mass')))
+        w.saveSnapshot('snapshot_' + model, params)
+        res.Write('res_' + model)
 
-    # Save fit results
-    outfile = TFile(outfile_name, 'RECREATE')
-    res_gauss_chi2.Write('res_gauss_chi2')
-    res_breitwigner_chi2.Write('res_breitwigner_chi2')
-    res_voigtian_chi2.Write('res_voigtian_chi2')
-    res_crystalball_chi2.Write('res_crystalball_chi2')
-    res_gauss_mle.Write('res_gauss_mle')
-    res_breitwigner_mle.Write('res_breitwigner_mle')
-    res_voigtian_mle.Write('res_voigtian_mle')
-    res_crystalball_mle.Write('res_crystalball_mle')
+    for model in list_of_models_mle:
+        res = w.pdf(model).fitTo(data_hist, RooFit.Save())
+        params = w.pdf(model).getParameters(RooArgSet(w.var('mass')))
+        w.saveSnapshot('snapshot_' + model, params)
+        res.Write('res_' + model)
+    
+    # res_gauss_chi2       = w.pdf('gauss_chi2').chi2FitTo(data_hist, RooFit.Save())
+    # res_breitwigner_chi2 = w.pdf('breitwigner_chi2').chi2FitTo(data_hist, RooFit.Save())
+    # res_voigtian_chi2    = w.pdf('voigtian_chi2').chi2FitTo(data_hist, RooFit.Save())
+    # res_crystalball_chi2 = w.pdf('crystalball_chi2').chi2FitTo(data_hist, RooFit.Save())
+    # res_gauss_mle        = w.pdf('gauss_mle').fitTo(data_hist, RooFit.Save())
+    # res_breitwigner_mle  = w.pdf('breitwigner_mle').fitTo(data_hist, RooFit.Save())
+    # res_voigtian_mle     = w.pdf('voigtian_mle').fitTo(data_hist, RooFit.Save())
+    # res_crystalball_mle  = w.pdf('crystalball_mle').fitTo(data_hist, RooFit.Save())
+
+    w.writeToFile(outfile_name)
     outfile.Close()
-
+    
     return
 
 
-def test():
+def test ():
     """Test functionality.
     """
     filename = '/home/submit/kyoon/CMSSW_10_6_27/src/Hrare/analysis/outname_mc10_Wcat.root'
     h = fetch_histogram(filename, 'ZH_HCandMass', 'HCandMass from mc10 file')
     fit(h, 'ZH', 'test_ZH_fit.root')
 
+    f = TFile('test_ZH_fit.root', 'READ')
+    w = f.workspace_ZH
+
+    models = ['gauss_chi2', 'breitwigner_chi2', 'voigtian_chi2', 'crystalball_chi2']
+    models += ['gauss_mle', 'breitwigner_mle', 'voigtian_mle', 'crystalball_mle']
+
+    for m in models:
+        plot_fit_results(w, 'data_ZH', 'snapshot_' + m, m, m + '.jpg')
+    
     return
 
 
-def plot_fit_results():
-    pass
+def plot_fit_results (workspace, data_name, snapshot_name, model_name, outfile_name):
+    """Plots fit results created by the fit function.
+
+    Creates TCanvas and split into two pads. In the first pad, the fitted model
+    is plotted on top of the data. In the second pad, the residuals are plotted.
+    The resulting TCanvas is saved to the given file path.
+
+    Args:
+        workspace: RooWorkspace containing the model
+        data_name: Name of the data in workspace
+        snapshot_name: Name of the snapshot to load
+        model_name: Name of the model to load
+        outfile_name: Name of the outfile to write to
+
+    Returns:
+        (None)
+    """
+    # Fit histogram
+    frame1 = workspace.var('mass').frame(RooFit.Title('Fit of \"{}\"'.format(model_name)))
+    workspace.data(data_name).plotOn(frame1)
+    workspace.pdf(model_name).plotOn(frame1, RooFit.LineColor(kRed),
+                                     RooFit.Name('fit'))
+    workspace.pdf(model_name).paramOn(frame1, RooFit.Layout(0.6, 0.88, 0.85))
+    frame1.getAttText().SetTextFont(43)
+    frame1.getAttText().SetTextSize(21)
+    
+    # Residual histogram
+    hresid = frame1.residHist()
+    frame2 = workspace.var('mass').frame(RooFit.Title('Residuals of \"{}\"'.format(model_name)))
+    frame2.addPlotable(hresid, 'P')
+
+    # Draw on canvas
+    gStyle.SetOptStat(0)
+    c = TCanvas('c', 'c')
+    c.SetCanvasSize(1500, 1500)
+
+    p1 = TPad('p1', 'p1', 0.05, 0.41, 0.95, 0.95)
+    p2 = TPad('p2', 'p2', 0.05, 0.05, 0.95, 0.39)
+
+    p1.cd()
+    frame1.Draw()
+    p1.Update()
+    
+    p2.cd()
+    frame2.Draw()
+    p2.Update()
+
+    c.cd()
+    p1.Draw()
+    p2.Draw()
+    c.Update()
+    c.Print(outfile_name)
+    
+    return
 
 
 if __name__ == '__main__':
