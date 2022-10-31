77dbfacd3f2e7e63fcf4b6063e20ceec043e9e7c
Kyungseop Yoon <kyoon@mit.edu>
2022-10-30 21:53:25 -0400

    first commit in this branch

---
+++ TMVA_Torch.py
@@ -0,0 +1,56 @@
+from ROOT import TMVA, TFile, TTree, TCut
+from subprocess import call
+from os.path import isfile
+
+import torch
+from torch import nn
+
+# Setup TMVA
+TMVA.Tools.Instance()
+TMVA.PyMethodBase.PyInitialize()
+
+outfile = 'torch_test.root'
+output = TFile.Open(outfile, 'RECREATE')
+
+factory = TMVA.Factory('TMVAClassification',
+                       output,
+                      '!V:!Silent:Color:DrawProgressBar:Transformations=G:AnalysisType=Classification')
+
+# Load data
+def load_data(cat='ggH', meson='rho', year=[2018], sgn_mc=[1027], bkc_mc=[6,7,8,9,10]): # TODO: move to utils module
+    """
+    Helper function to load data into TMVA.
+    
+    [Arguments]
+    cat: (string) name of the category, e.g. 'ggH', 'VBF',  'VBFlow', 'Zinv', etc.
+    meson: (string) name of the meson produced, e.g. 'phi', 'rho'
+    year: (list) year of the data/MC taken/produced
+    sgn_mc: (list) MC number(s) of the signal process; - for data
+    bkg_mc: (list) MC number(s) of the background process; - for data
+    """
+
+    top_level_dir = '/work/submit/kyoon/RareHiggs/data/'
+    file_format = top_level_dir + 'cat_{meson}/cat_{meson}_{cat}/test/test_mc{mc}_{cat}cat_{meson}Cat_{yr}.root'
+
+    signal_files = []
+    background_files = []
+    
+    for yr_ in year:
+        for mc_ in sgn_mc:
+            f = Tfile.Open(file_format.format(**{yr:yr_, mc:mc_, cat:cat, meson:meson}), 'READ')
+            f.SetDirectory(0)
+            signal_files.append(f)
+        for mc_ in bkg_mc:
+            f = Tfile.Open(file_format.format(**{yr:yr_, mc:mc_, cat:cat, meson:meson}), 'READ')
+            f.SetDirectory(0)
+            background_files.append(f)
+
+    return signal_files, background_files
+
+s, b = load_data()
+
+"""
+Dataloader = TMVA.DataLoader('Dataset')
+
+for sgn_file in signalfiles:   
+"""
