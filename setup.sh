a0ba3dae4e21e4e7e8d9167b0266b3480dccd4e2
Kyungseop Yoon <kyoon@mit.edu>
2022-11-15 10:33:24 -0500

    loading torch in rdataframe only works with the latest ROOT

--- setup.sh
+++ setup.sh
@@ -1,1 +1,2 @@
-source /cvmfs/sft.cern.ch/lcg/views/LCG_102/x86_64-centos7-gcc11-opt/setup.sh
+# source /cvmfs/sft.cern.ch/lcg/views/LCG_102/x86_64-centos7-gcc11-opt/setup.sh
+source /cvmfs/sft.cern.ch/lcg/views/dev3/latest/x86_64-centos7-gcc11-opt/setup.sh
