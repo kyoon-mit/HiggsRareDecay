0d78540afe1b58ff3abc82dbdf844511b89dfaf2
Kyungseop Yoon <kyoon@mit.edu>
2023-02-12 07:07:35 -0500

    updated env setup and added script to plot mass corr in mva disc

---
+++ macros/setup.sh
@@ -0,0 +1,3 @@
+# source /cvmfs/sft.cern.ch/lcg/views/LCG_102/x86_64-centos7-gcc11-opt/setup.sh
+# source /cvmfs/sft.cern.ch/lcg/views/dev3/latest/x86_64-centos7-gcc11-opt/setup.sh
+source /cvmfs/sft.cern.ch/lcg/views/LCG_102/x86_64-centos7-gcc8-opt/setup.sh
