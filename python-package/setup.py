d86621ea9bc4b412480007ef25b57fb585525fa2
Kyungseop Yoon <kyoon@mit.edu>
2023-09-10 18:20:26 -0400

    setup info

---
+++ python-package/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+setuptools.setup(name='kytools',
+version='0.0.2',
+description='K.Yoon\'s analysis tools',
+url='#',
+author='Kyungseop Yoon',
+install_requires=['uproot', 'pandas'],
+author_email='kyoon@mit.edu',
+packages=setuptools.find_packages(),
+zip_safe=False)
